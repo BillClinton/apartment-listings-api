c56863731f8424e567afbf4c73145cc8a3f7b3a8
BillClinton <bclinton@appunto.net>
2019-09-24 19:16:56 -0400

    Add read users endpoint

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = new express.Router();
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 
 /**
  * Login
@@ -14,38 +15,49 @@ router.post('/users/login', async (req, res) => {
     const token = await user.generateAuthToken();
     res.send({ user, token });
   } catch (e) {
-    //res.status(400).send();
-    res.status(400).send({ error: e.message });
+    res.status(400).send();
+  }
+});
+
+/**
+ * Logout
+ */
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(token => {
+      return token.token != req.token;
+    });
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(400).send();
   }
 });
 
 /**
  * Create user
  */
-// router.post('/users', async (req, res) => {
-//   const user = new User(req.body);
-
-//   try {
-//     await user.save();
-
-//     res.status(201).send({ user });
-//   } catch (e) {
-//     res.status(400).send({ error: e.message });
-//   }
-// });
-
-/**
- * Create user and login
- */
 router.post('/users', async (req, res) => {
   const user = new User(req.body);
 
   try {
     await user.save();
-    const token = await user.generateAuthToken();
-    res.status(201).send({ user, token });
+
+    res.status(201).send({ user });
   } catch (e) {
     res.status(400).send({ error: e.message });
+  }
+});
+
+/**
+ * Read users
+ */
+router.get('/users', auth, async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send(e);
   }
 });
 
