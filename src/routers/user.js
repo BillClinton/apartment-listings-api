7a2f2f7fc723a4b6e2c626d0984986cf6175e851
BillClinton <bclinton@appunto.net>
2019-09-25 01:37:41 -0400

    Begin converting comments from jsdoc to apidocjs

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,10 +1,35 @@
+/**
+ * Express router providing user related routes
+ */
 const express = require('express');
 const router = new express.Router();
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 
 /**
- * Login
+ * @api {post} /users/login Login
+ * @apiName LoginUser
+ * @apiGroup User
+ *
+ * @apiParam {JSON} body Email and password combination.
+ * @apiParamExample {json} Request-Example:
+ *     {
+ *       "email": "nfoles9@gmail.com",
+ *       "password": "eagles4133"
+ *     }
+ *
+ * @apiSuccess {Object} user User profile information
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "name": "Nick",
+ *       "surname": "Foles",
+ *       "email": "nfoles9@gmail.com",
+ *     }
+ *
+ * @apiError (Error 4xx) 400 Bad Request
+ *
  */
 router.post('/users/login', async (req, res) => {
   try {
@@ -20,7 +45,11 @@ router.post('/users/login', async (req, res) => {
 });
 
 /**
- * Logout
+ * Route that logs out current user.
+ * @name POST users/logout
+ * @member
+ * @instance
+ * @method
  */
 router.post('/users/logout', auth, async (req, res) => {
   try {
@@ -35,7 +64,11 @@ router.post('/users/logout', auth, async (req, res) => {
 });
 
 /**
- * Logout all sessions
+ * Route that logs out current user and destroys all user sessions.
+ * @name POST users/logoutAll
+ * @member
+ * @instance
+ * @method
  */
 router.post('/users/logoutAll', auth, async (req, res) => {
   try {
@@ -48,7 +81,12 @@ router.post('/users/logoutAll', auth, async (req, res) => {
 });
 
 /**
- * Create user
+ * Route that accepts and creates a user.
+ * @name POST /users
+ * @member
+ * @instance
+ * @method
+ * @param {Object} user - JSON Object representing new user to be created
  */
 router.post('/users', async (req, res) => {
   const user = new User(req.body);
@@ -63,7 +101,31 @@ router.post('/users', async (req, res) => {
 });
 
 /**
- * Read users
+ * @api {get} /users Request All Users
+ * @apiName GetUsers
+ * @apiGroup User
+ *
+ * @apiSuccess {Object[]} users An Array of user objects
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     [{
+ *       "name": "John",
+ *       "surname": "Doe",
+ *       "email": "johndoe@email.com"
+ *     },{
+ *       "name": "Elizabeth",
+ *       "surname": "Doe",
+ *       "email": "edoe@email.com"
+ *     }]
+ *
+ * @apiError (Error 5xx) 500 Internal Server Error
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 500 Internal Server Error
+ *     {
+ *       "error": "Error Message"
+ *     }
  */
 router.get('/users', auth, async (req, res) => {
   try {
@@ -75,18 +137,65 @@ router.get('/users', auth, async (req, res) => {
 });
 
 /**
- * Read current user
+ * @api {get} /user/me Request Current User
+ * @apiName GetProfile
+ * @apiGroup User
+ *
+ * @apiSuccess {Object} user User profile information
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "name": "John",
+ *       "surname": "Doe",
+ *       "email": "johndoe@email.com"
+ *     }
+ *
+ * @apiError (Error 5xx) 500 Internal Server Error
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 500 Internal Server Error
+ *     {
+ *       "error": "Error Message"
+ *     }
  */
 router.get('/users/me', auth, async (req, res) => {
   try {
     res.send(req.user);
   } catch (e) {
-    res.status(500).send(e);
+    res.status(500).send({ error: e.message });
   }
 });
 
 /**
- * Update user
+ * @api {patch} /users/:id Update User
+ * @apiName UpdateUser
+ * @apiGroup User
+ *
+ * @apiParam {String} id Users unique ID.
+ * @apiParam {JSON} body Updates to be applied to user.
+ * @apiParamExample {json} Request-Example:
+ *     {
+ *       "name": "Marco"
+ *     }
+ *
+ * @apiSuccess {Object} user User profile information
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "name": "John",
+ *       "surname": "Doe",
+ *       "email": "johndoe@email.com"
+ *     }
+ *
+ * @apiError (Error 4xx) 400 Invalid Updates Attempted
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "error": "Invalid updates attempted."
+ *     }
  */
 router.patch('/users/:id', auth, async (req, res) => {
   const updates = Object.keys(req.body);
@@ -96,7 +205,7 @@ router.patch('/users/:id', auth, async (req, res) => {
   );
 
   if (!isValidOperation) {
-    return res.status(400).send('error: invalid updates attempted');
+    return res.status(400).send({ error: 'Invalid updates attempted.' });
   }
 
   try {
@@ -111,7 +220,12 @@ router.patch('/users/:id', auth, async (req, res) => {
 });
 
 /**
- * Delete user
+ * Route that deletes a user.
+ * @name DELETE users/:id
+ * @member
+ * @instance
+ * @method
+ * @param {string} id - The user id.
  */
 router.delete('/users/:id', auth, async (req, res) => {
   try {
