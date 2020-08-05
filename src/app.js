d8bcf50aa597eab60806315d1b1c2d453c2c873c
BillClinton <bclinton@appunto.net>
2020-08-04 20:53:27 -0400

    enable CORS

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 require('./db/mongoose.js');
 
 const userRouter = require('./routers/user');
@@ -6,12 +7,13 @@ const apartmentRouter = require('./routers/apartment');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use(userRouter);
 app.use(apartmentRouter);
 
 // respond with 404 if root is requested
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send(404);
 });
 
